68cdb84e34483a4a5a5853d7db15c21e5c6911f3
Rajeshh <rajeshbese2@gmail.com>
2025-11-30 12:49:34 +0530

    primenumbers in a given range

--- prime or not.py
+++ prime or not.py
@@ -1,7 +1,11 @@
 def primeOrNot(num):
-    for i in range(2,num):
-        if num%i==0:
-            return False
-        else:
-            return True
-print(primeOrNot(5))
+    for i in range(1, num + 1):
+        cnt = 0
+        for j in range(1, i+1):
+            if i % j== 0:
+                cnt+=1
+        if cnt==2:
+            print(i)
+
+
+primeOrNot(50)
